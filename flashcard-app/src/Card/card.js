5af66330d8ee0cad063c473d1f54d5402d5396f9
Molly Graham <mollyclaire.graham@gmail.com>
2019-08-25 11:33:20 -0500

    Updated card and button styles.

--- flashcard-app/src/Card/card.js
+++ flashcard-app/src/Card/card.js
@@ -7,11 +7,11 @@ import "./card.css";
 const Card = (props) => (
     <div className="card-container">
         <div className="card">
-            <div className="front">
+            <div className="front cardBody">
                 <div className="question">{props.question}</div>
             </div>
             <div className="back">
-                <div className="answer">{props.answer}</div>
+                <div className="answer cardBody">{props.answer}</div>
             </div>
         </div>
         
