22a5a3e0c6b008b97330aa92a51afb967ac3981e
Molly Graham <mollyclaire.graham@gmail.com>
2019-09-03 15:49:18 -0500

    Updates

---
+++ precache-manifest.d447b5f4443bc83797cf3593248d3370.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38424e03d5fd7ff34d2d170ba3fdb86",
+    "url": "/Flashcard-App/index.html"
+  },
+  {
+    "revision": "fc58faa15856199b9d13",
+    "url": "/Flashcard-App/static/css/main.b47ca1ad.chunk.css"
+  },
+  {
+    "revision": "2cba5e7359492997fe68",
+    "url": "/Flashcard-App/static/js/2.7bb1cfc0.chunk.js"
+  },
+  {
+    "revision": "fc58faa15856199b9d13",
+    "url": "/Flashcard-App/static/js/main.b8e8ffc7.chunk.js"
+  },
+  {
+    "revision": "d12f9cf9525aa554b92a",
+    "url": "/Flashcard-App/static/js/runtime~main.c7c5fd80.js"
+  }
+]);
